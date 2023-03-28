66520191585c16eed346132b231110a20a1f12ee
lea-schwarz <lea.s@aon.at>
2023-03-28 15:42:33 +0200

    first code

---
+++ src/main.go
@@ -0,0 +1,19 @@
+// main.go
+
+package main
+
+import (
+	"os"
+	"net/http"
+)
+
+func main() {
+	a := App {}
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
+
+	http.Handle("/", a.Router)
+	http.ListenAndServe(":8010", nil)
+}
